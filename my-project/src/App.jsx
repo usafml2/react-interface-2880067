8ab887eab5ba8fc405808d7079bbd5eb2ac66b90
usafml2 <usafml2@gmail.com>
2024-08-28 13:28:13 -0400

    Course Completed
    Things basically work, but with the Vite installation, I may have Tailwind CSS problems making certain box sizes a little off from the proffs create-react-app way.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect, useCallback } from 'react'
-import { BiCalendar } from "react-icons/bi"
-import Search from "./components/Search"
-import AddAppointment from "./components/AddAppointment"
+
+import { useState, useEffect, useCallback}  from 'react'
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo"
 
 function App() {
@@ -9,36 +10,37 @@ function App() {
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
   let [sortBy, setSortBy] = useState("petName");
-  let [orderBy, setOrderBy] = useState("asc");
+  let [orderBy, setOrderBy] = useState('asc');
 
   const filteredAppointments = appointmentList.filter(
     item => {
       return (
-        item.petName.toLowerCase().includes(query.toLowerCase()) ||
-        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+        item.petName.toLowerCase().includes(query.toLowerCase())  ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase())  ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())  
       )
     }
   ).sort((a, b) => {
     let order = (orderBy === 'asc') ? 1 : -1;
     return (
-      a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
-        ? -1 * order : 1 * order
+      a[sortBy].toLowerCase() <  b[sortBy].toLowerCase() 
+       ? -1 * order : 1 * order
+      
     )
-  })
-
+})
   const fetchData = useCallback(() => {
     fetch('./data.json')
       .then(response => response.json())
       .then(data => {
         setAppointmentList(data)
       });
-  }, [])
+    }, [])
+
 
   useEffect(() => {
     fetchData()
-  }, [fetchData]);
-
+  }, [fetchData]);  
+  
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
@@ -55,17 +57,19 @@ function App() {
         onSortByChange={mySort => setSortBy(mySort)}
       />
 
+
       <ul className="divide-y divide-gray-200">
         {filteredAppointments
           .map(appointment => (
             <AppointmentInfo key={appointment.id}
               appointment={appointment}
               onDeleteAppointment={
-                appointmentId =>
-                  setAppointmentList(appointmentList.filter(appointment =>
+                appointmentId  =>
+                  setAppointmentList(appointmentList.filter(appointment => 
                     appointment.id !== appointmentId))
               }
-            />
+                />
+              
           ))
         }
       </ul>
